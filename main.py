de35475438a744dba5fd1dbf3bebcaa9f2596688
HicaroD <hdanrlley1@gmail.com>
2022-03-18 20:00:35 -0300

    Implementing view for returning player info as JSON

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify, make_response
+import scraper
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "<p>Hello, world</p>"
-
 @app.route("/<nickname>")
 def get_player(nickname):
-    return f"<p> Hello, {nickname}"
+    liquipedia_scraper = scraper.LiquipediaScraper(nickname)
+    player_description = {
+            "description": liquipedia_scraper.get_player_description(),
+    }
+    return make_response(jsonify(player_description))
 
 if __name__ == "__main__":
     app.run()
